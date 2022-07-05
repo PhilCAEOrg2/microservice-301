8781d9db7c184c8e290fe2e7bbd7d8e14cf65982
PhilCAEUser <mail@rwth>
2022-07-05 14:01:27 +0000

    test

--- app/src/main/java/i5/las2peer/services/res/classes.java
+++ app/src/main/java/i5/las2peer/services/res/classes.java
@@ -6,6 +6,57 @@ import org.json.simple.parser.ParseException;
 
 public class classes {
 
+    class User {
+
+    public User() {}
+    
+    private boolean loggedIn;
+
+    public void setloggedIn(boolean setValue) {
+        this.loggedIn = setValue;
+    }
+
+    public boolean getloggedIn() {
+        return this.loggedIn;
+    }
+    private int id;
+
+    public void setid(int setValue) {
+        this.id = setValue;
+    }
+
+    public int getid() {
+        return this.id;
+    }
+    private String username;
+
+    public void setusername(String setValue) {
+        this.username = setValue;
+    }
+
+    public String getusername() {
+        return this.username;
+    }
+
+    public JSONObject toJSON() {
+
+        JSONObject jo = new JSONObject();
+        jo.put("loggedIn", this.loggedIn); 
+        jo.put("id", this.id); 
+        jo.put("username", this.username); 
+
+        return jo;
+    }
+
+    public void fromJSON(String jsonString) throws ParseException {
+        JSONObject jsonObject = (JSONObject) JSONValue.parseWithException(jsonString);
+        this.loggedIn = (1 == ((Long) jsonObject.get("loggedIn")).intValue()); 
+        this.id = ((Long) jsonObject.get("id")).intValue(); 
+        this.username = (String) jsonObject.get("username"); 
+
+    }
+
+}
 
     
 }
